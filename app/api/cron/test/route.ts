24b446bab17c0000be626aeb84b0bc92c33acbcf
Chirath abeyamanna <chamodyachirath@gmail.com>
2025-10-08 20:08:00 +0530

    Fixed caching in corn jobs

--- app/api/cron/test/route.ts
+++ app/api/cron/test/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
+export const revalidate = 0; // Disable caching for cron routes
+
 export const GET = async (reqeust: NextRequest) => {
   console.log("[CRON] Auto sign-out cron job started");
   return NextResponse.json(
